572977819b441e606c884bf9aabf3751ed690c4e
raymcnd <89778453+raymcnd@users.noreply.github.com>
2023-08-08 13:51:29 +0700

    feat: apply authorization

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Controller = require("../controllers")
 const authentication = require('../middlewares/authentication')
+const authorization = require('../middlewares/authorization')
 
 router.get('/', (req, res) => {
     res.send('Hello World!')
@@ -12,15 +13,15 @@ router.post("/register", authentication, Controller.registerAdmin);
 router.post("/login", Controller.loginAdmin);
 
 router.get("/posts", authentication, Controller.readPosts);
-router.post("/posts", authentication, Controller.addPost);
+router.post("/posts", authentication, authorization, Controller.addPost);
 router.get("/posts/:id", authentication, Controller.readPostById);
-router.put("/posts/:id", authentication, Controller.editPost);
-router.delete("/posts/:id", authentication, Controller.deletePost);
+router.put("/posts/:id", authentication, authorization, Controller.editPost);
+router.delete("/posts/:id", authentication, authorization, Controller.deletePost);
 router.get("/categories", authentication, Controller.readCategories);
-router.post("/categories", authentication, Controller.addCategory);
+router.post("/categories", authentication,  authorization, Controller.addCategory);
 router.get("/categories/:id", authentication, Controller.readCategoryById);
-router.put("/categories/:id", authentication, Controller.editCategory);
-router.delete("/categories/:id", authentication, Controller.deleteCategory);
+router.put("/categories/:id", authentication, authorization, Controller.editCategory);
+router.delete("/categories/:id", authentication, authorization, Controller.deleteCategory);
 
 // Routing Client User
 router.get("/pub/posts", Controller.readPosts);
